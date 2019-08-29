62e292ddef09ec5c7322860f6c767b405f438070
Administrator <Administrator@DESKTOP-H4Q429A.lan>
2019-08-29 19:53:05 +0800

    run1.0

--- test1/src/main/java/controller/testController.java
+++ test1/src/main/java/controller/testController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class testController {
+	@RequestMapping("index")
+	public void index(String txt,ModelMap m) {
+		m.put("info",txt); 
+	}
 
 }
